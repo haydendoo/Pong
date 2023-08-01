96bcd52428b6e755e6ac2692a1f37daa27e39df5
Hayden How <100998299+haydendoo@users.noreply.github.com>
2023-08-01 01:36:34 +0000

    basic window

--- include/RenderWindow.hpp
+++ include/RenderWindow.hpp
@@ -5,5 +5,9 @@
 
 class RenderWindow {
 public:
+    RenderWindow(const char *p_title, int p_w, int p_h);
+    void cleanUp();
 private:
+    SDL_Window *window;
+    SDL_Renderer *renderer;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <SDL2/SLD_image.h>
 #include <iostream>
 
+#include "RenderWindow.hpp"
+
 int main(int agrv, char *args[]) {
     if(SDL_Init(SDL_INIT_VIDEO) > 0) 
         std::cout << "SDL_Init HAS FAILED. Error: " << SDL_GetError() << std::endl;
@@ -9,6 +11,21 @@ int main(int agrv, char *args[]) {
     if(!(IMG_Init(IMG_INIT_PNG))) 
         std::cout << "IMG_Init HAS FAILED. Error: " << SDL_GetError() << std::endl;
 
-    std::cout << "Hello World" << std::endl;
+    RenderWindow window("Pong", 1280, 720);
+    SDL_Event event;
+
+    bool gameRunning = true;
+
+    while(gameRunning) {
+        while(SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                gameRunning = false;
+            }
+        }
+    }
+
+    window.cleanUp();
+    SDL_Quit();
+
     return 0;
 }
